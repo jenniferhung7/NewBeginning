d6e5d6c296254e388766d5680f16068e1044b1b8
Jennifer <jenniferhung7@gmail.com>
2023-12-20 14:36:01 +0800

    Initial Commit

---
+++ Homework/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by JenniferHung on 2023/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+class CapsuleBorderButton: UIButton {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.borderWidth = 2
+        layer.cornerRadius = bounds.midY
+        layer.borderColor = titleColor(for: .normal)?.cgColor
+    }
+}
